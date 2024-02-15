1ce54c55e2b7c0a90f55b0eae738233a5414ba21
Paulo André <pauloandre.franquini@gmail.com>
2024-02-15 16:05:43 -0300

    Alguns comentários para melhorar o entendimento do código e aprimoramento das funções dias_de_folga e nome_das_colunas

--- criador_escalas.py
+++ criador_escalas.py
@@ -8,6 +8,8 @@ class Funcionario:
         self.lista = []
 
 
+# Essa função adiciona o nome à lista e marca os dias de trabalho e de folga para poder ser usada
+# pela biblioteca tabulate.
 def dias_de_folga(frequencia, funcionario):
 
     if funcionario.primeira_folga is None:
@@ -15,16 +17,23 @@ def dias_de_folga(frequencia, funcionario):
         funcionario.lista.append(funcionario.nome)
         return dias_de_folga(funcionario.primeira_folga, funcionario)
 
-    elif funcionario.primeira_folga > 1 and len(funcionario.lista) < 31:
+    elif funcionario.primeira_folga >= 1 and len(funcionario.lista) < 32:
         for i in range(frequencia):
-            funcionario.lista.append("o")
+            if len(funcionario.lista) == 32:
+                break
+            else:
+                funcionario.lista.append("o")
         for i in range(1):
-            funcionario.lista.append("x")
+            if len(funcionario.lista) == 32:
+                break
+            else:
+                funcionario.lista.append("x")
         return dias_de_folga(funcionario.primeira_folga, funcionario)
 
 
-def name_das_colunas(lista_col_nomes):
-   for i in range(31):
+# Faz uma lista com os dias para poder usada pela biblioteca tabulate.
+def nome_das_colunas(lista_col_nomes):
+   for i in range(1,32):
        lista_col_nomes.append(i)
 
 
@@ -37,7 +46,7 @@ roberto = Funcionario("Roberto")
 dias_de_folga(4, roberto)
 
 col_nomes = ["Funcionários"]
-name_das_colunas(col_nomes)
+nome_das_colunas(col_nomes)
 
 
 data = [paulo.lista, roberto.lista]
